aaf6568d498f9ec43aa80a66edcf0ff46f65d060
Aj <Ajaykuma24@gmail.com>
2021-07-27 20:26:41 +0200

    updated

--- Apache_kafka/Wrking/Kafka_docker/kafka-installation7/kafka-image/start.sh
+++ Apache_kafka/Wrking/Kafka_docker/kafka-installation7/kafka-image/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mkdir -p /usr/local/kafka/kafka-logs
+mkdir -p /opt/kafka_2.11-2.2.1/kafka-logs
 
 kafka_2.11-2.2.1/bin/kafka-server-start.sh kafka_2.11-2.2.1/config/server.properties
--- Apache_kafka/Wrking/Kafka_docker/kafka-installation7/zk-image/start.sh
+++ Apache_kafka/Wrking/Kafka_docker/kafka-installation7/zk-image/start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 mkdir -p /opt/zookeeper-3.4.6/state/zookeeper
-echo 1 > /usr/local/zookeeper/state/zookeeper/myid
+echo 1 > /opt/zookeeper-3.4.6/state/zookeeper/myid
 
 zookeeper-3.4.6/bin/zkServer.sh start
-sleep 5
-
