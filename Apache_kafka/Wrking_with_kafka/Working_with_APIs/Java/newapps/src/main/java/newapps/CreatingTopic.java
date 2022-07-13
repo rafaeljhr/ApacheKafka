222f8fcd7c6918d7e2ea4dced70860ef39a6633b
Aj <ajaykuma24@gmail.com>
2022-07-13 13:40:53 +0200

    updated

---
+++ Apache_kafka/Wrking_with_kafka/Working_with_APIs/Java/newapps/src/main/java/newapps/CreatingTopic.java
@@ -0,0 +1,25 @@
+package newapps;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class CreatingTopic {
+  public static void main(String[] args) throws ExecutionException, InterruptedException {
+      Properties config = new Properties();
+      config.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "n1:9092");
+      AdminClient admin = AdminClient.create(config);
+      //creating new topic
+      System.out.println("-- creating --");
+      NewTopic newTopic = new NewTopic("Topic10", 1, (short) 1);
+      admin.createTopics(Collections.singleton(newTopic));
+
+      //listing
+      System.out.println("-- listing --");
+      admin.listTopics().names().get().forEach(System.out::println);
+      
+  }
+}
