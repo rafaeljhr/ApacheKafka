b285c46acaed2f0e354141b350aaf6add47f7186
aj <ajaykuma24@gmail.com>
2024-07-16 08:12:44 +0200

    updated

--- Apache_kafka/Wrking_with_kafka/Setup_Instructions/kafka-clus.sh
+++ Apache_kafka/Wrking_with_kafka/Setup_Instructions/kafka-clus.sh
@@ -11,16 +11,17 @@ nohup kafka-server-start.sh /usr/local/kafka/sncluster/server2.properties &
 sleep 5s
 nohup kafka-server-start.sh /usr/local/kafka/sncluster/server3.properties &
 sleep 10s
-echo stat | nc ch1 2181 | grep Mode
+echo stat | nc localhost 2181 | grep Mode
 sleep 2s
-echo stat | nc ch1 2182 | grep Mode
+echo stat | nc localhost 2182 | grep Mode
 sleep 2s
-echo stat | nc ch1 2183 | grep Mode  
+echo stat | nc localhost 2183 | grep Mode  
 sleep 10s
 echo dump |nc localhost 2181 | grep brokers
 sleep 5s
-kafka-topics.sh --describe --bootstrap-server ch1:9092
-kafka-topics.sh --bootstrap-server ch1:9092 --create --topic Test1 --partitions 3 --replication-factor 3
-kafka-topics.sh --bootstrap-server ch1:9092 --list
-kafka-topics.sh --describe --bootstrap-server ch1:9092
+kafka-topics.sh --delete --topic Test1 --bootstrap-server localhost:9092
+kafka-topics.sh --describe --bootstrap-server localhost:9092
+kafka-topics.sh --bootstrap-server localhost:9092 --create --topic Test1 --partitions 3 --replication-factor 3
+kafka-topics.sh --bootstrap-server localhost:9092 --list
+kafka-topics.sh --describe --bootstrap-server localhost:9092
 
