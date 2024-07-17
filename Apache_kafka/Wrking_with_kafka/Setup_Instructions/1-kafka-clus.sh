e20fa1eb768be94125c969a1421bf503a2f7c4d8
aj <ajaykuma24@gmail.com>
2024-07-17 09:04:53 +0200

    updated

--- Apache_kafka/Wrking_with_kafka/Setup_Instructions/1-kafka-clus.sh
+++ Apache_kafka/Wrking_with_kafka/Setup_Instructions/1-kafka-clus.sh
@@ -6,8 +6,13 @@ sudo chown -R hdu:hdu kafka*
 cp -r kafka_2.11-2.2.1/sncluster/ kafka/
 sed -i 's/server.2=ch1:2888:3888/server.2=ch1:2889:3889/g' /usr/local/kafka/sncluster/zookeeper1.properties 
 sed -i 's/server.3=ch1:2888:3888/server.2=ch1:2890:3890/g' /usr/local/kafka/sncluster/zookeeper1.properties 
+echo 4lw.commands.whitelist=* >> /usr/local/kafka/sncluster/zookeeper1.properties
 sed -i 's/server.2=ch1:2888:3888/server.2=ch1:2889:3889/g' /usr/local/kafka/sncluster/zookeeper2.properties     
 sed -i 's/server.3=ch1:2888:3888/server.2=ch1:2890:3890/g' /usr/local/kafka/sncluster/zookeeper2.properties
+echo 4lw.commands.whitelist=* >> /usr/local/kafka/sncluster/zookeeper2.properties
 sed -i 's/server.2=ch1:2888:3888/server.2=ch1:2889:3889/g' /usr/local/kafka/sncluster/zookeeper3.properties     
 sed -i 's/server.3=ch1:2888:3888/server.2=ch1:2890:3890/g' /usr/local/kafka/sncluster/zookeeper3.properties
-
+echo 4lw.commands.whitelist=* >> /usr/local/kafka/sncluster/zookeeper3.properties
+cat /usr/local/kafka/sncluster/zookeeper1.properties
+cat /usr/local/kafka/sncluster/zookeeper2.properties
+cat /usr/local/kafka/sncluster/zookeeper3.properties
